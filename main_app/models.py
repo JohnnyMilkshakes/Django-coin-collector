069dc82e0f9f7676f2ab9a91a3d7dfc440ead4d6
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-10 13:38:33 -0400

    add __str__ method to coin model

--- main_app/models.py
+++ main_app/models.py
@@ -9,3 +9,6 @@ class Coin(models.Model):
     denomination = models.CharField(max_length=100)
     # country = models.ForeignKey()
     # owner = models.ForeignKey()
+    
+    def __str__(self):
+        return f"Name: {self.name}, Year: {self.year}, metal: {self.metal_type}"
