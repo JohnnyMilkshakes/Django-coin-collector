29a72ea06a0be15a7ab1c2b85bf114b94503d575
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-10 13:25:23 -0400

    create coin model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Coin(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.IntegerField(max_length=100)
+    metal_type = models.CharField()
+    description = models.TextField(max_length=500)
+    denomination = models.CharField(max_length=100)
+    country = models.ForeignKey()
+    owner = models.ForeignKey()
