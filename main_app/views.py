12eec822645fe6f3f5d9e113e73e18fa1d6b7b99
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-10 13:54:01 -0400

    create coin views for urls to point to

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import Coin
+from .serializers import CoinSerializer
 
 # Define the home view
 class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the coin-collector api home route!'}
     return Response(content)
+
+class CoinList(generics.ListCreateAPIView):
+  queryset = Coin.objects.all()
+  serializer_class = CoinSerializer
+
+class CoinDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Coin.objects.all()
+  serializer_class = CoinSerializer
+  lookup_field = 'id'
