03de1d53a50005b06a2d2219e76aa97f249a0ae6
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-12 07:34:34 -0400

    creat transaction serializer

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Coin
+from .models import Coin, Transaction
 
 class CoinSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coin
         fields = '__all__'
+        
+class TransactionSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Transaction
+    fields = '__all__'
+    read_only_fields = ('coin',)
