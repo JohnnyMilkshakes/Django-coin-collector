a5ecef632542d82bd8076510d714511e63b5d146
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-12 07:28:44 -0400

    add Transaction model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+# A tuple of 2-tuples
+TRANSACTION_TYPE = (
+    ('B', 'Buy'),
+    ('S', 'Sell'),
+)
 # Create your models here.
 class Coin(models.Model):
     name = models.CharField(max_length=100)
@@ -12,3 +17,26 @@ class Coin(models.Model):
     
     def __str__(self):
         return f"Name: {self.name}, Year: {self.year}, metal: {self.metal_type}"
+    
+    
+# Add new Feeding model below Cat model
+class Transaction(models.Model):
+  date = models.DateField('Transaction Date')
+  type = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=TRANSACTION_TYPE,
+    # set the default value for transaction type to be 'B' or buy
+    default=TRANSACTION_TYPE[0][0]
+  )
+  price = models.FloatField()
+  # Create a coin_id FK
+  coin = models.ForeignKey(Coin)
+  
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+  # change the default sort
+  class Meta:
+    ordering = ['-date']
