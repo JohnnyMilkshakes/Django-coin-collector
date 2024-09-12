6bb99ad01dafc6dd8efa2eb3a8c16c7c1260bdad
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-12 11:46:40 -0400

    create transaction views

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('coins/', CoinList.as_view(), name='coin-list'),
     path('coins/<int:id>/', CoinDetail.as_view(), name='coin-detail'),
+    
     path('coins/<int:coin_id>/feedings/', TransactionListCreate.as_view(), name='transaction-list-create'),
 	path('coins/<int:coin_id>/feedings/<int:id>/', TransactionDetail.as_view(), name='transaction-detail'),
 ]
