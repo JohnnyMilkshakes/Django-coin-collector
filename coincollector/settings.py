9f1087313d9a1be0c12216c7644422f166ae5c55
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-13 09:25:13 -0400

    update settings

--- coincollector/settings.py
+++ coincollector/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4%@k1yj5t9q_sn^ove(qdr=abuv$v!-794v79)e9%*wa+%jnvr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
